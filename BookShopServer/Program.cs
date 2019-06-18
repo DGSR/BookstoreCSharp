00e2f5be8c748568f6683952d8e35fe65d588ae7
Herman Islamov <33521824+DGSR@users.noreply.github.com>
2019-06-18 22:15:53 +0300

    Add files via upload

--- BookShopServer/Program.cs
+++ BookShopServer/Program.cs
@@ -2,19 +2,34 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+using System.Linq;
 
 namespace BookShopServer
 {
     class Program
     {
+        public static IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+        public static IPAddress ipAddr = ipHost.AddressList[0];
+        public static int port = 11000;
+        public static IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+        public static Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        public static string path = Path.GetFullPath(@".\Data\base-islamov.mdf");
         static void Main(string[] args)
         {
             // конфигурация
-            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
             // создаем сокет Tcp/Ip
             Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(LocalDB)\MSSQLLocalDB",
+                AttachDBFilename = path,
+                IntegratedSecurity = true
+            };
+            IDbConnection connection = new SqlConnection(builder.ConnectionString);
+
             // назначаем сокет и слушаем входящие сокеты
             try
             {
@@ -25,11 +40,37 @@ namespace BookShopServer
                     // программа приостанавливается, ожидая входящее соединение
                     Socket handler = sListener.Accept();
                     string data = "";
+                    string reply = "";
                     byte[] bytes = new byte[1024];
                     int bytesRec = handler.Receive(bytes);
                     data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
-                    Console.Write("Received Message: " + data + "\nSupport: ");
-                    string reply =  Console.ReadLine();
+                    Console.Write("Received Data: " + data + "\n");
+                    connection.Open();
+                    IDbCommand command = new SqlCommand(data);
+                    command.Connection = connection;
+                    IDataReader reader = command.ExecuteReader();
+                    if (reader.FieldCount != 0)
+                    {
+                        while (reader.Read())
+                        {
+                            if (reader.FieldCount == 1)
+                                reply += reader.GetValue(0) + ";";
+                            else
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                    reply += reader.GetValue(i) + ",";
+                                reply += ";";
+                            }
+                            //for(int i)
+                        }
+                        reply.Trim();
+                        if (reply.Contains(";"))
+                            reply = reply.Remove(reply.LastIndexOf(';'), 1);
+                    }
+                    reader.Close();
+                    command.Dispose();
+                    connection.Close();
+                    Console.WriteLine(reply);
                     byte[] msg = Encoding.UTF8.GetBytes(reply);
                     handler.Send(msg);
                     if (data.IndexOf("<TheEnd>") > -1)
